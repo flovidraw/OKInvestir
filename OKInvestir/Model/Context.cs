7f8394f4cc94c2997d61a7508f14d83669af8cbd
JunyangHE <nathanhejunyang@gmail.com>
2017-06-01 14:30:11 +0200

    Deleted useless data for entityFw, added readme2

--- OKInvestir/Model/Context.cs
+++ OKInvestir/Model/Context.cs
@@ -3,32 +3,21 @@
     using System;
     using System.Data.Entity;
     using MySql.Data.Entity;
-    using System.Linq;
     using System.Configuration;
-    using System.ComponentModel.DataAnnotations;
     using System.Collections.Generic;
     using System.Globalization;
-    using System.Data.Entity.Validation;
 
     [DbConfigurationType(typeof(MySqlEFConfiguration))]
 
     public class Context : DbContext
     {
-        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
-        //使用“Context”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
-        //“OKInvestir.model.Context”数据库。
-        // 
-        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“Context”
-        //连接字符串。
         public Context()
             : base(GetConnectionString())
         //: base("name=Context")
         {
             Database.SetInitializer<Context>(new SeedingDataInitializer());
         }
-
-        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
-        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+        
 
         public DbSet<Account> Accounts { get; set; }
         public DbSet<BoughtProduct> BoughtProducts { get; set; }
@@ -52,39 +41,11 @@
             return connString;
         }
 
-
-        /**
-         * Override savechanges method, to show more DbEntityValidationException detail
-         */
-        /*public override int SaveChanges()
-        {
-            try
-            {
-                return base.SaveChanges();
-            }
-            catch (DbEntityValidationException ex)
-            {
-                // Retrieve the error messages as a list of strings.
-                var errorMessages = ex.EntityValidationErrors
-                        .SelectMany(x => x.ValidationErrors)
-                        .Select(x => x.ErrorMessage);
-
-                // Join the list to a single string.
-                var fullErrorMessage = string.Join("; ", errorMessages);
-
-                // Combine the original exception message with the new one.
-                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
-
-                // Throw a new DbEntityValidationException with the improved exception message.
-                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
-            }
-        }*/
-
     }
 
     // Seeding initializer, add seed rows in db each creation of db.
-    //public class SeedingDataInitializer : CreateDatabaseIfNotExists<Context>
-    public class SeedingDataInitializer : DropCreateDatabaseIfModelChanges<Context>
+    public class SeedingDataInitializer : CreateDatabaseIfNotExists<Context>
+    //public class SeedingDataInitializer : DropCreateDatabaseIfModelChanges<Context>
     //public class SeedingDataInitializer : DropCreateDatabaseAlways<Context>
     {
         protected override void Seed(Context context)
@@ -101,18 +62,7 @@
                 new Client { IdCardNumber = "000000000001", FirstName = "clt", LastName = "1" },
                 new Client { IdCardNumber = "000000000002", FirstName = "clt", LastName = "2" },
                 new Client { IdCardNumber = "000000000003", FirstName = "clt", LastName = "3" },
-                new Client { IdCardNumber = "000000000004", FirstName = "clt", LastName = "4" },
-                new Client { IdCardNumber = "000000000005", FirstName = "clt", LastName = "5" },
-                new Client { IdCardNumber = "000000000006", FirstName = "clt", LastName = "6" },
-                new Client { IdCardNumber = "000000000007", FirstName = "clt", LastName = "7" },
-                new Client { IdCardNumber = "000000000008", FirstName = "Shannon", LastName = "Wycoff" },
-                new Client { IdCardNumber = "000000000009", FirstName = "Marita", LastName = "Allmon" },
-                new Client { IdCardNumber = "000000000010", FirstName = "Marinda", LastName = "Kwok" },
-                new Client { IdCardNumber = "000000000011", FirstName = "Billy", LastName = "Petersen" },
-                new Client { IdCardNumber = "000000000012", FirstName = "Rutha", LastName = "Lemmon" },
-                new Client { IdCardNumber = "000000000013", FirstName = "Florine", LastName = "Redden" },
-                new Client { IdCardNumber = "000000000014", FirstName = "Vicki", LastName = "Gaulke" },
-                new Client { IdCardNumber = "000000000015", FirstName = "Karyl", LastName = "Bow" }
+                new Client { IdCardNumber = "000000000004", FirstName = "clt", LastName = "4" }
             };
             var AccountList = new List<Account>
             {
@@ -138,15 +88,7 @@
                 new Product { Name = "Product 2", Description = "Description 2", ProductStatus = 2 },
                 new Product { Name = "pdt3", Description = "d3", ProductStatus = 1 },
                 new Product { Name = "pdt4", Description = "d4", ProductStatus = 1 },
-                new Product { Name = "pdt5", Description = "d5", ProductStatus = 2 },
-                new Product { Name = "pdt6", Description = "d6", ProductStatus = 2 },
-                new Product { Name = "pdt7", Description = "d7", ProductStatus = 2 },
-                new Product { Name = "pdt8", Description = "d8", ProductStatus = 3 },
-                new Product { Name = "pdt9", Description = "d9", ProductStatus = 3 },
-                new Product { Name = "pdt10", Description = "d10", ProductStatus = 3 },
-                new Product { Name = "余额宝", Description = "余额宝是支付宝打造的余额理财服务。" +
-                    "\n把钱转入余额宝即购买了由天弘基金提供的天弘余额宝货币市场基金，可获得收益。" +
-                    "\n余额宝内的资金还能随时用于网购支付，灵活提取。", ProductStatus = 1 }
+                new Product { Name = "pdtd", Description = "dd", ProductStatus = 1 }
             };
             var SillInterestList = new List<SillInterest>
             {
@@ -160,7 +102,8 @@
                 new SillInterest { ProductID = 2, Sill = 10000M, Interest = 14M },
                 new SillInterest { ProductID = 3, Sill = 10, Interest = 3M },
                 new SillInterest { ProductID = 3, Sill = 100M, Interest = 5M },
-                new SillInterest { ProductID = 5, Sill = 233M, Interest = 23.3M },
+                new SillInterest { ProductID = 4, Sill = 233M, Interest = 23.3M },
+                new SillInterest { ProductID = 5, Sill = 233M, Interest = 23.3M }
             };
             var TimeInterestList = new List<TimeInterest>
             {
@@ -174,7 +117,7 @@
                 new TimeInterest { ProductID = 3, Time = 48, Interest = 19M },
                 new TimeInterest { ProductID = 4, Time = 24, Interest = 10M },
                 new TimeInterest { ProductID = 4, Time = 96, Interest = 50M },
-                new TimeInterest { ProductID = 5, Time = 233, Interest = 23.3M }
+                new TimeInterest { ProductID = 5, Time = 96, Interest = 50M }
             };
             var BoughtPdtList = new List<BoughtProduct>
             {
@@ -182,19 +125,19 @@
                     StartDate = DateTime.ParseExact("24/12/2016", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                     EndDate = DateTime.ParseExact("24/01/2019", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                     BuyingDate = DateTime.ParseExact("04/01/2017", "dd/MM/yyyy", CultureInfo.InvariantCulture),
-                    Price = 12345, SettlementPrice = 22345, 
+                    Price = 12345, SettlementPrice = 22345,
                     Client = CltList[0], Product = PdtList[0]},
                 new BoughtProduct { FinalInterest = 23M, BoughtStatus = 1,
                     StartDate = DateTime.ParseExact("02/02/2017", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                     EndDate = DateTime.ParseExact("02/01/2018", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                     BuyingDate = DateTime.ParseExact("05/01/2017", "dd/MM/yyyy", CultureInfo.InvariantCulture),
-                    Price = 6666, SettlementPrice = 8888, 
+                    Price = 6666, SettlementPrice = 8888,
                     Client = CltList[0], Product = PdtList[1]},
                 new BoughtProduct { FinalInterest = 5M, BoughtStatus = 1,
                     StartDate = DateTime.ParseExact("03/11/2017", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                     EndDate = DateTime.ParseExact("03/11/2019", "dd/MM/yyyy", CultureInfo.InvariantCulture),
                     BuyingDate = DateTime.ParseExact("11/02/2017", "dd/MM/yyyy", CultureInfo.InvariantCulture),
-                    Price = 20000, SettlementPrice = 23333, 
+                    Price = 20000, SettlementPrice = 23333,
                     Client = CltList[1], Product = PdtList[2]},
                 new BoughtProduct { FinalInterest = 0.5M, BoughtStatus = 1,
                     StartDate = DateTime.ParseExact("10/07/2018", "dd/MM/yyyy", CultureInfo.InvariantCulture),
---
+++ README2.txt
@@ -0,0 +1,12 @@
+Our project use MySQL Database.
+Parameters for Database connection are in ./OKInvestir/Model/DbParam.cs
+
+Database generate by Entity framework code first, no SQL Script needed.
+You can choose SeedingDataInitializer in ./OKInvestir/Model/Concext.cs line 47 to line 49.
+
+Some data for testing will be inserted in to DB when database is created.
+
+Github: https://github.com/NathanKun/OKInvestir/
+
+HE Junyang
+SONG Yuzhou
